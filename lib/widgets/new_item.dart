24460e2c000de63b553f79fcfd87986e71555bbb
admin <william0129@gmail.com>
2024-04-03 17:29:45 +0800

    add new item button on appbar

---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget{
+  const NewItem ({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItemState();
+  }
+}
+
+class _NewItemState extends State<NewItem>{
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a bew item')
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Text('The form'),
+      ),
+    );
+  }
+}
