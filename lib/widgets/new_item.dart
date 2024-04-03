c44336d83f2653774c80b2fa5b5972108bdd053c
admin <william0129@gmail.com>
2024-04-03 19:04:03 +0800

    extract entered form values

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
 
 class NewItem extends StatefulWidget{
   const NewItem ({super.key});
@@ -12,9 +13,14 @@ class NewItem extends StatefulWidget{
 
 class _NewItemState extends State<NewItem>{
   final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
 
   void _saveItem() {
-    _formKey.currentState!.validate();
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    };
   } 
   
   @override
@@ -44,6 +50,9 @@ class _NewItemState extends State<NewItem>{
                   }
                   return null;
                 },
+                onSaved: (value) {
+                  _enteredName = value!;
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -54,22 +63,26 @@ class _NewItemState extends State<NewItem>{
                         label: Text('Quantity'),
                       ),
                       keyboardType: TextInputType.number,
-                      initialValue: '1',
+                      initialValue: _enteredQuantity.toString(),
                       validator: (value) {
-                  if (value == null 
-                    || value.isEmpty 
-                    || int.tryParse(value) == null
-                    || int.tryParse(value)! <= 0
-                  ) {
-                    return 'Must be a postive integer';            
-                  }
-                  return null;
-                },
+                        if (value == null 
+                          || value.isEmpty 
+                          || int.tryParse(value) == null
+                          || int.tryParse(value)! <= 0
+                        ) {
+                          return 'Must be a postive integer';            
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
+                      },
                     ),
                   ),
                   const SizedBox(width: 8),
                   Expanded(
                     child: DropdownButtonFormField(
+                      value: _selectedCategory,    //DropdownBottonField does not support initialValue, use value instead.
                       items: [
                         for (final category in categories.entries)
                         DropdownMenuItem(
@@ -87,7 +100,9 @@ class _NewItemState extends State<NewItem>{
                           ),
                         ),
                       ], 
-                      onChanged: (value){},
+                      onChanged: (value){
+                        _selectedCategory = value!;
+                      },
                     ),
                   ),
                 ],
