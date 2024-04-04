637fad84386e1fac1444e06f1aecae7b35baa783
admin <william0129@gmail.com>
2024-04-04 15:23:50 +0800

    add error message if http request fail

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -17,6 +17,7 @@ class GroceryList extends StatefulWidget{
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem>_groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
 
   @override
@@ -29,6 +30,11 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https(
       'learn-b62f7-default-rtdb.asia-southeast1.firebasedatabase.app', 'shopping-list.json');
     final response = await http.get(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data.  Please try again later.';
+      });
+    }
     final Map<String, dynamic>listData = json.decode(response.body);
     final List<GroceryItem>loadedItems = [];
     for (final item in listData.entries) {
@@ -101,7 +107,9 @@ class _GroceryListState extends State<GroceryList> {
       );
     }
 
-
+  if (_error != null) {
+    content = Center(child: Text(_error!));
+  }
 
 
     return Scaffold(
